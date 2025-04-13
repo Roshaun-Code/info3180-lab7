49722065a8fa33b1ab304a9bde35417e50836168
Roshaun Marshall <roshaunmarshall@gmail.com>
2025-04-13 14:59:05 -0500

    Exercise 2 completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
-from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import views
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Import models after initializing db to avoid circular imports
+from app import models
